d0d0cc5e81df696d1a5ebd1fe9560a3e09a8d4b2
malin <malleballe90@gmail.com>
2024-02-21 12:42:17 +0100

    ingredients and description

--- src/Recipies.tsx
+++ src/Recipies.tsx
@@ -5,17 +5,37 @@ export interface Recipie {
     id: string;
     image: string;
     title: string;
+    ingredients: string [];
+    description: string;
     
 }
 export const mockedRecipies: Recipie [] = [{
 
         id: "1",
         image: fishSoup,
-        title: "Het fisksoppa"
+        title: "Het fisksoppa",
+        ingredients: ["200g lax", "200g torsk", "1 burk krossade tomater", "2 Vitlöks klyftor", "Creme fraise"],
+        description: "börja med att steka lök och vitlök i en kastrull, när löken blivit bjuk och fått färg häller du i en burk med krossade tomater och sedan lika mycket vatten"
     },
     {
         id: "2",
         image: peanutSallad,
-        title: "Fusion sallad"
+        title: "Fusion sallad",
+        ingredients: ["200g Jordnötter","2 Salladslökar"],
+        description: "börja med pajdegen, blanda ihop mjöl, smör, vatten och salt till en jämn deg, tryck ut i en pajform och nagga botten med en gaffel"
+    },/*
+    {
+        id: "3",
+        image: 
+        title: "Medelhavspaj",
+        ingredients: ["",""],
+        description: ""
     },
+    {
+        id: "4",
+        image: peanutSallad,
+        title: "Fusion sallad",
+        ingredients: ["",""],
+        description: ""
+    },*/
 ];
